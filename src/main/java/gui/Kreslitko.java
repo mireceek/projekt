3addd2b714298522f45e61d9c4e5f761c2c004a0
pavlimi2 <pavlimi2@uhk.cz>
2024-03-27 13:49:32 +0100

    Lala

--- src/main/java/gui/Kreslitko.java
+++ src/main/java/gui/Kreslitko.java
@@ -1,7 +1,17 @@
 package gui;
 import javax.swing.*;
-public class Kreslitko extends JFrame {
+public class Kreslitko/* extends JFrame*/ {
 
+    private JFrame hlavniOkno = new JFrame();
+    private JToolBar nastrojPanelu;
+public Kreslitko(){
+}
+private void inializaceKomponent(){
+    /*JFrame hlavniOkno = new JFrame();*/
+     hlavniOkno = new JFrame();
+     nastrojPanelu = new JToolBar();
+
+}
 
 
     public static void main(String[] args){
